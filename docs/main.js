f64599c9c774c1d1ba09c17e3532a798a3092164
Somrad Sharma <somrad.sharma@student.ehb.be>
2021-12-21 13:37:12 +0100

    simple fetch added

--- docs/main.js
+++ docs/main.js
@@ -1,1 +1,27 @@
-console.log("hello world");
+window.onload = function () {
+  getData();
+}
+
+ async function getData(){
+  await fetch(`https://swapi.dev/api/people`)
+  .then(resp => resp.json())
+  .then(data => {
+  console.log(data.results);
+  showData(data)
+  })
+}
+
+function showData(data){
+  data.results.forEach(data => {
+    let htmlString = `
+  <h1>Title: ${data.name}</h1>
+  <h2>Episode ${data.height}</h2>
+  `
+  document.getElementById('people').innerHTML= htmlString;
+  });
+  
+}
+
+
+
+
