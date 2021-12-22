4788051d8ea1e327ab6f7bf2f6177e2e5afe5f2d
Somrad Sharma <somrad.sharma@student.ehb.be>
2021-12-22 16:53:25 +0100

    added event listener

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 let list = [];
 let people= [];
 
+async function getData() {
+  await fetch('https://swapi.dev/api/people')
+  .then(resp => resp.json())
+  .then (data => {
+      list = data.results;
+      // LOOP 
+      list.forEach(starWars =>{
+          fetch(starWars.url)
+          .then (resp => resp.json())
+          .then(dataObj => {
+              people.push(dataObj);
+  
+          });
+      });
+  
+  });
+}
+console.log(people);
+
+
 window.onload = () => {
   console.log("page loaded");
   getData();
@@ -8,70 +28,35 @@ window.onload = () => {
   setTimeout(buildList, 1000);
 
   function buildList(){
-    // document.querySelector('.btn').forEach(item =>{
-    //   item.addEventListener('click', event =>{
-    //     //get id 
-    //     let id = event.target.id;
-    //     console.log(id);
-    //     //search in array
-    //     // let p = people.find(el => el.id == id);
-    //     // console.log(p);
-    //   })
-    // })
+    people.forEach(data => {
+      const peopleContainer = document.getElementById("list-data");
 
-    let html = '';
-    // order the list
-    people.sort(function(a,b){
-      return a.id - b.id
+      const peopleEl = document.createElement('div');
+
+      peopleEl.classList.add('people');
+      const innerHtml = `
+      <div class="swPerson">
+      <h2>${data.name}</h2>
+
+      <a href="#" id="${data.url}" class="btn btn-primary ">Add to list</a>
+      </div>
+      `;
+  
+      peopleEl.innerHTML = innerHtml;
+      peopleContainer.appendChild(peopleEl)
     })
 
-    for (let p of people) {
-      html += 
-     ` <div class="swPerson">
-        <h2>${p.name}</h2>
-         <button id="add-button">Add</button>
-         </div>`
-    }
-    document.getElementById('list-data').innerHTML = html;
+    document.querySelectorAll('.btn').forEach(item =>{
+      item.addEventListener('click', event =>{
+        //get id 
+        let id = event.target.url;
+        //search in array
+        let p = people.find(el => el.id == id);
+        console.log(p);
+    
+      })
+    })
   }
 }
 
-console.log(people);
-
-// async function getData() {
-//   await fetch('https://swapi.dev/api/people')
-//   .then(resp => resp.json())
-//   .then (data => {
-//       list = data.results;
-//       // LOOP 
-//       list.forEach(starWars =>{
-//           fetch(starWars.url)
-//           .then (resp => resp.json())
-//           .then(dataObj => {
-//               people.push(dataObj);
-  
-//           });
-//       });
-  
-//   });
-// }
-
-
-async function getData(){
-  //get the list
-  await fetch('https://swapi.dev/api/people')
-  .then(response => {
-      return response.json();
-  }).then(data => {
-      list = data.results;
-      //Loop over the list to get each pokemon
-      for(let element of list){
-          fetch(element.url).then(response => {
-              return response.json();
-           }).then(data => {
-               people.push(data);
-           })
-      }
-  });
-}
 
